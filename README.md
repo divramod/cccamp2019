b4a9ea7fbd6bf9e6bdf13b43dbba241d7858ee9c
divramod <divramod@gmail.com>
2019-08-21 14:58:35 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Install Camp2019 Network
+```sh
+sh <(curl https://raw.githubusercontent.com/divramod/cccamp2019/master/networkmanager.sh)
+```
--- networkmanager.sh
+++ networkmanager.sh
@@ -1,1 +1,8 @@
-#!/bin/sh 
+#!/bin/sh
+echo "Start importing camp network connection"
+curl -o Camp2019.nmconnection https://raw.githubusercontent.com/divramod/cccamp2019/master/Camp2019.nmconnection
+sudo rm -f /etc/NetworkManager/system-connections/Camp2019.nmconnection
+sudo mv Camp2019.nmconnection /etc/NetworkManager/system-connections
+sudo chmod 600 /etc/NetworkManager/system-connections/Camp2019.nmconnection
+sudo ls -lisa /etc/NetworkManager/system-connections
+echo "Finished importing camp network connection"
